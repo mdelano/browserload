98d49fe37171b980eb0c521e34b20c1cc3dc85fd
Alex Nauda <alex@alexnauda.com>
2014-05-30 17:17:47 -0400

    upload screenshot to S3 if exists, based on $AWS_ACCESS_KEY, $AWS_SECRET_KEY, and $S3_BUCKET

--- loop.bash
+++ loop.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
 while true; do
+    runId=`cat /proc/sys/kernel/random/uuid`
+    screenshot=$runId.png
     now=`date +%s%3N`
-    casperjs --resourceLoadThreshold=1000 --targetUrl=https://ql.mediasilo.com/#ql/537e7f2be4b0b22ab7f24bc8 tests/quicklinks.js
+    casperjs --resourceLoadThreshold=1000 --runId=$runId --screenshotPath=$screenshot --targetUrl=https://ql.mediasilo.com/#ql/537e7f2be4b0b22ab7f24bc8 tests/quicklinks.js
     echo "[info] [loop.bash] finished in $((`date +%s%3N` - now)) ms"
+
+    # upload the screenshot to S3 if it exists
+    if [ -e $screenshot ]; then
+        echo -n $AWS_SECRET_KEY > AWS_SECRET_KEY
+        lib/s3-bash/s3-put -k $AWS_ACCESS_KEY -s AWS_SECRET_KEY -T $screenshot /$S3_BUCKET/$screenshot
+    fi
 done
