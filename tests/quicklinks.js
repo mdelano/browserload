ab26d3cd78ec7a013669c9e648c4935f435d51d2
mdelano <mike@mediasilo.com>
2014-05-30 09:30:36 -0400

    Added timer for page resource loads

--- tests/quicklinks.js
+++ tests/quicklinks.js
@@ -1,6 +1,47 @@
 var assets = [];
-var casper = require('casper').create();
+var resourcesTime = [];
+var casper = require('casper').create({
+    verbose: true,
+    logLevel: 'debug'
+});
+var utils  = require('utils');
+
 var targetUrl = casper.cli.get('targetUrl')
+
+casper.on('load.started', function(err) {
+    casper.log("started");
+});
+
+casper.on('load.started', function(err) {
+    casper.log("finished");
+});
+
+casper.on('resource.requested', function(resource) {
+    var date_start = new Date();
+ 
+    resourcesTime[resource.id] = {
+        'id': resource.id,
+        'id_received': '', /* to debug */
+        'start': date_start.getTime(),
+        'end': -1,
+        'time': -1,
+        'status': resource.status,
+        'url': resource.url,
+        'url_received': '' /* to debug */
+    }
+});
+ 
+casper.on('resource.received', function(resource) {
+    var date_end = new Date();
+ 
+    resourcesTime[resource.id]['end']  = date_end.getTime();
+    resourcesTime[resource.id]['time'] = resourcesTime[resource.id]['end'] - resourcesTime[resource.id]['start'];
+ 
+    /* to debug and compare */
+    resourcesTime[resource.id]['id_received']  = resource.id;
+    resourcesTime[resource.id]['url_received'] = resource.url;
+ 
+});
 
 function getAssets() {
     var assets = document.querySelectorAll('.ttWrapper');
@@ -25,7 +66,9 @@ casper.then(function() {
 });
 
 casper.run(function() {
+    utils.dump(resourcesTime);
     // echo results in some pretty fashion
     this.echo(assets.length + ' assets found:');
     this.echo(' - ' + assets.join('\n - ')).exit();
+
 });
