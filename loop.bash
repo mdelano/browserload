d37b88561953fb0b8f7e5cfc3c9cdf2562cd3c6c
Alex Nauda <alex@alexnauda.com>
2014-05-30 15:25:42 -0400

    fix bug: print elapsed millis instead of nanos

--- loop.bash
+++ loop.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    now=`date +%s%N`
+    now=`date +%s%3N`
     casperjs --resourceLoadThreshold=1000 --targetUrl=https://ql.mediasilo.com/#ql/537e7f2be4b0b22ab7f24bc8 tests/quicklinks.js
-    echo "finished in $((`date +%s%N` - now))"
+    echo "finished in $((`date +%s%3N` - now)) ms"
 done
