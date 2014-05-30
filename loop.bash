72332598f67004972f0a15eb3e2c9a7c8299f529
Alex Nauda <alex@alexnauda.com>
2014-05-30 17:26:38 -0400

    log more when screenshot exists

--- loop.bash
+++ loop.bash
@@ -4,11 +4,13 @@ while true; do
     screenshot=$runId.png
     now=`date +%s%3N`
     casperjs --resourceLoadThreshold=1000 --runId=$runId --screenshotPath=$screenshot --targetUrl=https://ql.mediasilo.com/#ql/537e7f2be4b0b22ab7f24bc8 tests/quicklinks.js
-    echo "[info] [loop.bash] finished in $((`date +%s%3N` - now)) ms"
 
     # upload the screenshot to S3 if it exists
     if [ -e $screenshot ]; then
+        echo "[info] [loop.bash $runId] finished in $((`date +%s%3N` - now)) ms; screenshot to S3 /$S3_BUCKET/$screenshot"
         echo -n $AWS_SECRET_KEY > AWS_SECRET_KEY
         lib/s3-bash/s3-put -k $AWS_ACCESS_KEY -s AWS_SECRET_KEY -T $screenshot /$S3_BUCKET/$screenshot
+    else
+        echo "[info] [loop.bash $runId] finished in $((`date +%s%3N` - now)) ms"
     fi
 done
