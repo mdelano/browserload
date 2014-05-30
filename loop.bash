73fe89624e866641bbe87de64412eb4d1c545995
Alex Nauda <alex@alexnauda.com>
2014-05-30 15:19:30 -0400

    print elapsed millis of each looped execution when finished

--- loop.bash
+++ loop.bash
@@ -1,2 +1,6 @@
 #!/bin/bash
-while true; do casperjs --resourceLoadThreshold=1000 --targetUrl=https://ql.mediasilo.com/#ql/537e7f2be4b0b22ab7f24bc8 tests/quicklinks.js; done
+while true; do
+    now=`date +%s%N`
+    casperjs --resourceLoadThreshold=1000 --targetUrl=https://ql.mediasilo.com/#ql/537e7f2be4b0b22ab7f24bc8 tests/quicklinks.js
+    echo "finished in $((`date +%s%N` - now))"
+done
