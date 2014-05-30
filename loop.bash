60d421fc7bfb1f283028039e8af8642aa2d1526f
Alex Nauda <alex@alexnauda.com>
2014-05-30 15:30:30 -0400

    enrich log line

--- loop.bash
+++ loop.bash
@@ -2,5 +2,5 @@
 while true; do
     now=`date +%s%3N`
     casperjs --resourceLoadThreshold=1000 --targetUrl=https://ql.mediasilo.com/#ql/537e7f2be4b0b22ab7f24bc8 tests/quicklinks.js
-    echo "finished in $((`date +%s%3N` - now)) ms"
+    echo "[info] [loop.bash] finished in $((`date +%s%3N` - now)) ms"
 done
